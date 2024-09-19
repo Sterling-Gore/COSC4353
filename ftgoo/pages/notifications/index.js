d6e2eb24fdc0cab302db837ab025904da19cdf11
Jasark0 <jasonyen2004@gmail.com>
2024-09-19 14:11:22 -0500

    Feat: Notifications page added array mapping, started on eventspage

--- ftgoo/pages/notifications/index.js
+++ ftgoo/pages/notifications/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 export default function Notifications() {
+  const notificationsArray = [
+    { eventNum: 1, time: "1:22 PM", eventStatus: "This event is now coming up, please show up quickly, it starts soon." },
+    { eventNum: 2, time: "1:01 PM", eventStatus: "This event was now cancelled. No need to come and visit." },
+    { eventNum: 3, time: "1:00 PM", eventStatus: "New events that match your profile, please check them out in the events page." },
+  ];
+
   return (
     <div style={styles.container}>
       <div style={styles.containerCover}>
@@ -13,47 +19,24 @@ export default function Notifications() {
             </a>
           </div>
         </nav>
-
         <div style={styles.notificationsContainer}>
-          <p style={styles.notificationsText}>
+          <div style={styles.notificationsText}>
             Notifications
-          </p>
-
-          <div style={styles.eventContainer}> 
-            <div style={styles.topLeftEventText}>   
-                Event 1 - Reminder
-            </div>
-            <div style={styles.bottomLeftEventText}>
-                1:22 PM
-            </div>
-            <div style={styles.rightEventText}>
-                This event is now coming up, please show up quickly, it starts soon.
-            </div>
-          </div>
-          
-          <div style={styles.eventContainer}> 
-            <div style={styles.topLeftEventText}>   
-                Event 2 - Update
-            </div>
-            <div style={styles.bottomLeftEventText}>
-                1:01 PM
-            </div>
-            <div style={styles.rightEventText}>
-                This even was now cancelled. No need to come and visit.
-            </div>
           </div>
 
-          <div style={styles.eventContainer}> 
-            <div style={styles.topLeftEventText}>   
-                Event 3 - Match
-            </div>
-            <div style={styles.bottomLeftEventText}>
-                1:00 PM
-            </div>
-            <div style={styles.rightEventText}>
-                New events that match your profile, please check them out in events page.
-            </div>
-          </div>
+          {notificationsArray.map((notification) => (
+              <div style={styles.eventContainer} key={notification.eventNum}>
+                <div style={styles.topLeftEventText}>
+                  Event {notification.eventNum} - Reminder
+                </div>
+                <div style={styles.bottomLeftEventText}>
+                  {notification.time}
+                </div>
+                <div style={styles.rightEventText}>
+                  {notification.eventStatus}
+                </div>
+              </div>
+            ))}
         </div>
       </div>
     </div>
@@ -132,10 +115,10 @@ const styles = {
       marginBottom: "5vh",
       width: "80%",
       height: "12vh",
-      border: "1px solid black",  // Ensure the border is black and solid
+      border: "1px solid black", 
       display: "flex",
-      alignItems: "flex-start", // Align items to the top
-      position: "relative", // For absolute positioning of child elements
+      alignItems: "flex-start", 
+      position: "relative", 
     },
     topLeftEventText: {
       textAlign: "left",
@@ -160,7 +143,7 @@ const styles = {
       position: "absolute",
       top: 0,
       right: 0,
-      maxWidth: "400px",
+      maxWidth: "415px",
       lineHeight: "1.5",
     },
   };
