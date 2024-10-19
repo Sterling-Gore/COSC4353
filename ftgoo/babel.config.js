9e88a69709b0ae7b9bda049a3b624db765f51831
Sterling-Gore <155609125+Sterling-Gore@users.noreply.github.com>
2024-10-18 22:25:00 -0500

    Fix: fixed the babel.config to allow us to run the website and do unit testing too

--- ftgoo/babel.config.js
+++ ftgoo/babel.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  presets: [["@babel/preset-env", { targets: { node: "current" } }]],
+  presets: [
+    process.env.NODE_ENV === "test"
+      ? ["@babel/preset-env", { targets: { node: "current" } }]
+      : "next/babel", // Use the Next.js preset in all environments except testing
+  ],
 };
+
+/*module.exports = {
+  presets: [["@babel/preset-env", { targets: { node: "current" } }]],
+};*/
